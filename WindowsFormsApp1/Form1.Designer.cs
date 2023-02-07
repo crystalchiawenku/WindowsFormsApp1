9bd1000735d5fe9b89087ac623506bb33e4ef0e5
crystal.ku <inochiyoshi@hotmail.com>
2023-02-07 18:13:57 +0800

    加入專案檔案。

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,217 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.testDataSet = new WindowsFormsApp1.TestDataSet();
+            this.testBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.testTableAdapter = new WindowsFormsApp1.TestDataSetTableAdapters.testTableAdapter();
+            this.idDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.testtimeDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.classDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.titleDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.summaryDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.articleDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.authorDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.hitnoDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.getnoDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.emailnoDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.approvedDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.testDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.testBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.idDataGridViewTextBoxColumn,
+            this.testtimeDataGridViewTextBoxColumn,
+            this.classDataGridViewTextBoxColumn,
+            this.titleDataGridViewTextBoxColumn,
+            this.summaryDataGridViewTextBoxColumn,
+            this.articleDataGridViewTextBoxColumn,
+            this.authorDataGridViewTextBoxColumn,
+            this.hitnoDataGridViewTextBoxColumn,
+            this.getnoDataGridViewTextBoxColumn,
+            this.emailnoDataGridViewTextBoxColumn,
+            this.approvedDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.testBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(35, 144);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(473, 150);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // testDataSet
+            // 
+            this.testDataSet.DataSetName = "TestDataSet";
+            this.testDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // testBindingSource
+            // 
+            this.testBindingSource.DataMember = "test";
+            this.testBindingSource.DataSource = this.testDataSet;
+            // 
+            // testTableAdapter
+            // 
+            this.testTableAdapter.ClearBeforeFill = true;
+            // 
+            // idDataGridViewTextBoxColumn
+            // 
+            this.idDataGridViewTextBoxColumn.DataPropertyName = "id";
+            this.idDataGridViewTextBoxColumn.HeaderText = "id";
+            this.idDataGridViewTextBoxColumn.Name = "idDataGridViewTextBoxColumn";
+            this.idDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // testtimeDataGridViewTextBoxColumn
+            // 
+            this.testtimeDataGridViewTextBoxColumn.DataPropertyName = "test_time";
+            this.testtimeDataGridViewTextBoxColumn.HeaderText = "test_time";
+            this.testtimeDataGridViewTextBoxColumn.Name = "testtimeDataGridViewTextBoxColumn";
+            // 
+            // classDataGridViewTextBoxColumn
+            // 
+            this.classDataGridViewTextBoxColumn.DataPropertyName = "class";
+            this.classDataGridViewTextBoxColumn.HeaderText = "class";
+            this.classDataGridViewTextBoxColumn.Name = "classDataGridViewTextBoxColumn";
+            // 
+            // titleDataGridViewTextBoxColumn
+            // 
+            this.titleDataGridViewTextBoxColumn.DataPropertyName = "title";
+            this.titleDataGridViewTextBoxColumn.HeaderText = "title";
+            this.titleDataGridViewTextBoxColumn.Name = "titleDataGridViewTextBoxColumn";
+            // 
+            // summaryDataGridViewTextBoxColumn
+            // 
+            this.summaryDataGridViewTextBoxColumn.DataPropertyName = "summary";
+            this.summaryDataGridViewTextBoxColumn.HeaderText = "summary";
+            this.summaryDataGridViewTextBoxColumn.Name = "summaryDataGridViewTextBoxColumn";
+            // 
+            // articleDataGridViewTextBoxColumn
+            // 
+            this.articleDataGridViewTextBoxColumn.DataPropertyName = "article";
+            this.articleDataGridViewTextBoxColumn.HeaderText = "article";
+            this.articleDataGridViewTextBoxColumn.Name = "articleDataGridViewTextBoxColumn";
+            // 
+            // authorDataGridViewTextBoxColumn
+            // 
+            this.authorDataGridViewTextBoxColumn.DataPropertyName = "author";
+            this.authorDataGridViewTextBoxColumn.HeaderText = "author";
+            this.authorDataGridViewTextBoxColumn.Name = "authorDataGridViewTextBoxColumn";
+            // 
+            // hitnoDataGridViewTextBoxColumn
+            // 
+            this.hitnoDataGridViewTextBoxColumn.DataPropertyName = "hit_no";
+            this.hitnoDataGridViewTextBoxColumn.HeaderText = "hit_no";
+            this.hitnoDataGridViewTextBoxColumn.Name = "hitnoDataGridViewTextBoxColumn";
+            // 
+            // getnoDataGridViewTextBoxColumn
+            // 
+            this.getnoDataGridViewTextBoxColumn.DataPropertyName = "get_no";
+            this.getnoDataGridViewTextBoxColumn.HeaderText = "get_no";
+            this.getnoDataGridViewTextBoxColumn.Name = "getnoDataGridViewTextBoxColumn";
+            // 
+            // emailnoDataGridViewTextBoxColumn
+            // 
+            this.emailnoDataGridViewTextBoxColumn.DataPropertyName = "email_no";
+            this.emailnoDataGridViewTextBoxColumn.HeaderText = "email_no";
+            this.emailnoDataGridViewTextBoxColumn.Name = "emailnoDataGridViewTextBoxColumn";
+            // 
+            // approvedDataGridViewTextBoxColumn
+            // 
+            this.approvedDataGridViewTextBoxColumn.DataPropertyName = "approved";
+            this.approvedDataGridViewTextBoxColumn.HeaderText = "approved";
+            this.approvedDataGridViewTextBoxColumn.Name = "approvedDataGridViewTextBoxColumn";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(35, 24);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(481, 38);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(441, 90);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(549, 326);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.testDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.testBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private TestDataSet testDataSet;
+        private System.Windows.Forms.BindingSource testBindingSource;
+        private TestDataSetTableAdapters.testTableAdapter testTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn idDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn testtimeDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn classDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn titleDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn summaryDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn articleDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn authorDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn hitnoDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn getnoDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn emailnoDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn approvedDataGridViewTextBoxColumn;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+//----自己寫的（宣告)----
+using System.Configuration;//加入參考
+//using System.Data;
+using System.Data.SqlClient;
+//----自己寫的（宣告)----
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: 這行程式碼會將資料載入 'testDataSet.test' 資料表。您可以視需要進行移動或移除。
+            this.testTableAdapter.Fill(this.testDataSet.test);
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //=======微軟SDK文件的範本=======
+
+            //----上面已經事先寫好NameSpace --  using System.Web.Configuration; ----     
+            // Web.Config設定檔裡面的資料庫連結字串（ConnectionString），此連線名為 testConnectionString
+            SqlConnection Conn = new SqlConnection(ConfigurationManager.ConnectionStrings["WindowsFormsApp1.Properties.Settings.TestConnectionString"].ConnectionString);
+
+            //== 第一，連結資料庫。
+            Conn.Open();   //---- 開啟連結。這時候才連結DB
+
+            //== 第二，執行SQL指令。
+            SqlDataReader dr = null;
+            SqlCommand cmd = new SqlCommand("select [id],[test_time],[summary],[author] from [test] with(nolock)", Conn);
+            dr = cmd.ExecuteReader();   //---- 執行SQL指令（Select，搜尋、查詢）取出資料
+
+            //==第三，自由發揮，把執行後的結果呈現到畫面上。
+            //GridView1.DataSource = dr;
+            //GridView1.DataBind();    //--資料繫結
+
+            ////==也可以自己寫迴圈，展示每一筆記錄與其中的欄位==
+            while (dr.Read())
+            {
+                //Response.Write(dr["author"] + "<br />");
+                //Label1.Text += dr["author"] + "<br />";
+                textBox1.Text += dr["author"] + "/r/n";
+            }
+
+            // == 第四，釋放資源、關閉資料庫的連結。       
+            if (dr != null)
+            {
+                cmd.Cancel();
+                //----關閉DataReader之前，一定要先「取消」SqlCommand
+                //參考資料： http://blog.darkthread.net/blogs/darkthreadtw/archive/2007/04/23/737.aspx
+                dr.Close();
+            }
+            if (Conn.State == ConnectionState.Open)
+            {
+                Conn.Close();
+            }
+
+
+        }
+    }
+}
